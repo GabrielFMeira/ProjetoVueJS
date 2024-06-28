2d362022c27bf73d7df69bedfd3514b2d0ae8c65
Gabriel da Fonseca Meira <gabriel.meira@agcapital.local>
2024-06-28 17:55:43 -0300

    sistema de pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,22 +11,26 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Nome do cliente</div>
-                <div>pão</div>
-                <div>carne</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{burger.id}}</div>
+                <div>{{burger.nome}}</div>
+                <div>{{burger.pao}}</div>
+                <div>{{burger.carne}}</div>
                 <div>
                     <ul>
-                        <li>item</li>
-                        <li>item2</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{ opcional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
-                    <select name="status" class="status">
+                    <select name="status" class="status" @change="updateBurger($event, burger.id)">
                         <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
+                            {{ s.tipo }}
+                        </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -35,7 +39,47 @@
 
 <script>
     export default {
-        name: "Dashboard"
+        name: "Dashboard",
+        data () {
+            return{
+                burgers: null,
+                burger_id: null,
+                status: []
+            }
+        },
+        methods: {
+            async getPedidos() {
+                const req = await fetch("http://localhost:3000/burgers");
+                const data = await req.json();
+                this.burgers = data;
+                this.getStatus();
+            },
+            async getStatus() {
+                const req = await fetch("http://localhost:3000/status");
+                const data = await req.json();
+                this.status = data;
+            },
+            async deleteBurger(id) {
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "DELETE"
+                });
+                const res = await req.json();
+                this.getPedidos();
+            },
+            async updateBurger(event, id) {
+                const option = event.target.value;
+                const dataJson = JSON.stringify({ status: option });
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "PATCH",
+                    headers: {"COntent-Type": "application/json"},
+                    body: dataJson
+                });
+                const res = await req.json();
+            }
+        },
+        mounted () {
+            this.getPedidos();
+        }
     }
 </script>
 
